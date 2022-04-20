def111eab469012fc676c6e63f2ec543bf0acd14
antonin0306 <84185944+antonin0306@users.noreply.github.com>
2022-04-20 16:56:58 -0300

    for
    usei o "console.log" para mandar um numero certo de "weight" sem precisar repetir o comando; (fiz isso usando ".length") descobri a média tbm.

--- Aula 11/atividade 1/sketch.js
+++ Aula 11/atividade 1/sketch.js
@@ -1,8 +1,13 @@
 var weight = [35,38,42,45,43,34,36,41,48,32];
-
+var soma = 0
+var media = 0
 function setup() {
   createCanvas(400,400);
-
+for (var i = 0; i < weight.length; i++) {
+  soma = soma + weight[i];
+}
+media = soma/weight.length
+console.log (media)
 }
 
 function draw() 
