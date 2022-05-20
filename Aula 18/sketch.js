d02ae741980ca2d2adbfa20cec7c15b44439e4ff
antonin0306 <84185944+antonin0306@users.noreply.github.com>
2022-05-20 18:59:03 -0300

    classe

---
+++ Aula 18/sketch.js
@@ -0,0 +1,12 @@
+var antony;
+function setup() {
+  createCanvas(400,400);
+antony = new Student ("antony", "12", "7", "vermelho")
+antony.show()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ Aula 18/student.js
@@ -0,0 +1,16 @@
+
+class Student
+{
+    constructor (nome,idade,serie,corfavo) {
+        this.nome = nome
+        this.idade = idade
+        this.serie = serie
+        this.corfavo = corfavo
+    }
+    show () {
+        console.log (this.nome,this.idade,this.serie,this.corfavo)
+    }
+}
+
+
+
